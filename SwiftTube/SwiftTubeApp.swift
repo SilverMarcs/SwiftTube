0ce86a80c63934d67b02bbee0f324b1cbd483fe8
Zabir Raihan <zabirraihan@icloud.com>
2025-06-20 17:06:23 +0600

    Initial Commit

---
+++ SwiftTube/SwiftTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTubeApp.swift
+//  SwiftTube
+//
+//  Created by Zabir Raihan on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
