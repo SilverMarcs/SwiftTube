d64cb6ee549cb90d7c1892360b2b7737537d5f81
Zabir Raihan <zabirraihan@icloud.com>
2025-06-21 00:43:42 +0600

    Cleanup

--- SwiftTube/ContentView.swift
+++ SwiftTube/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var accountManager = AccountManager.shared
+    @State private var selection: Tabs = .videos
     
     var body: some View {
-        FeedView()
+        TabView(selection: $selection) {
+            Tab("Feed", systemImage: "video", value: .videos) {
+                VideoFeedTab()
+            }
+            
+            Tab("Subscriptions", systemImage: "person.2", value: .subscriptions) {
+                NavigationStack {
+                       SubscriptionsTab()
+                           .navigationTitle("Subscriptions")
+                   }
+            }
+            
+            Tab("Settings", systemImage: "gearshape.fill", value: .settings) {
+                SettingsView()
+            }
+            
+//            Tab(value: .search, role: .search) {
+//                SearchTab()
+//            }
+                
+        }
+        .tabViewStyle(.sidebarAdaptable)
+        #if !os(macOS)
+        .tabBarMinimizeBehavior(.onScrollDown)
+        #endif
+    }
+    
+    enum Tabs {
+        case videos
+        case subscriptions
+        case settings
     }
 }
