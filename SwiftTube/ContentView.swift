df4e2f9b59f49972682743ea49bd3e2d6af3b094
Zabir Raihan <zabirraihan@icloud.com>
2025-09-27 02:52:43 +0800

    Initial Commit

---
+++ SwiftTube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTube
+//
+//  Created by Zabir Raihan on 27/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTube/SwiftTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTubeApp.swift
+//  SwiftTube
+//
+//  Created by Zabir Raihan on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
